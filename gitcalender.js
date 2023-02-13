d222738a1ace2a1b16fc4c137a4535e3e6c705db
shanukajain <jain.shanuka@gmail.com>
2023-02-13 20:59:55 +0530

    correct code

---
+++ gitcalender.js
@@ -0,0 +1,12 @@
+
+  GitHubCalendar(".calendar", "shanukajain");
+
+  // or enable responsive functionality:
+  GitHubCalendar(".calendar", "shanukajain", { responsive: true });
+
+  // Use a proxy
+  GitHubCalendar(".calendar", "shanukajain", {
+     proxy (username) {
+       return fetch(`https://your-proxy.com/github?user=shanukajain`)
+     }
+  }).then(r => r.text())
